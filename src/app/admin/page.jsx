6179e88a3783e0c873eab10bc9c8b4fbb59d162b
Leopoldo <122998221+poldogs@users.noreply.github.com>
2024-01-13 20:09:09 +0100

    commit

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './adminPage.module.css';
 import AdminPosts from '../../components/adminPosts/AdminPosts';
 import AdminComments from '../../components/adminComments/AdminComments';
