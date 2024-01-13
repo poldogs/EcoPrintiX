da45e04daf3aae158bacd3e23c28dd84fbc876ab
Leopoldo <122998221+poldogs@users.noreply.github.com>
2024-01-13 19:27:50 +0100

    commit

--- src/components/adminComments/AdminComments.jsx
+++ src/components/adminComments/AdminComments.jsx
@@ -1,66 +1,52 @@
-'use client';
+import useSWR from 'swr';
 import styles from "./adminComments.module.css";
-import { useState, useEffect } from 'react';
 
+const fetcher = url => fetch(url).then(res => res.json());
 
 const AdminComments = () => {
+  const { data: comments, mutate } = useSWR('https://ecoprintix.vercel.app/api/adminComments', fetcher);
 
-    const [comments, setComments] = useState([]);
-    
-    const fetchData = async () => {
-      const res = await fetch(`https://ecoprintix.vercel.app/api/adminComments`, {
-        cache: "no-store",
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`https://ecoprintix.vercel.app/api/adminComments/${id}`, {
+        method: 'DELETE',
       });
 
-      if (!res.ok) {
-        throw new Error("Failed");
+      if (!response.ok) {
+        throw new Error('Error deleting comment');
       }
 
-      const json = await res.json();
-      setComments(json);
-    };
+      mutate();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
-    useEffect(() => {
-      fetchData();
-    }, []);
+  if (!comments) {
+    return <div>Loading...</div>;
+  }
 
-    const handleDelete = async (id) => {
-        try {
-          const response = await fetch(`https://ecoprintix.vercel.app/api/adminComments/${id}`, {
-            method: 'DELETE',
-          });
-      
-          if (!response.ok) {
-            throw new Error('Error deleting comment');
-          }
-      
-          setComments(comments.filter((comment) => comment.id !== id));
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      };
-
-    return(
-        <div>
-            <h2 className={styles.listTitle}>Comments List</h2>
-          {comments.map((comment) => (
-            <div key={comment.id} className={styles.comment}>
-              <h2 className={styles.commentUser}>{comment.userEmail}</h2>
-              <div
-                className={styles.commentContent}
-                dangerouslySetInnerHTML={{ __html: comment.desc }}
-              />
-              At: <div
-                className={styles.commentAt}
-                dangerouslySetInnerHTML={{ __html: comment.postSlug }}
-              />
-              <button onClick={() => handleDelete(comment.id)} className={styles.deleteButton}>
-                Delete
-              </button>
-            </div>
-          ))}
+  return(
+    <div>
+      <h2 className={styles.listTitle}>Comments List</h2>
+      {comments.map((comment) => (
+        <div key={comment.id} className={styles.comment}>
+          <h2 className={styles.commentUser}>{comment.userEmail}</h2>
+          <div
+            className={styles.commentContent}
+            dangerouslySetInnerHTML={{ __html: comment.desc }}
+          />
+          At: <div
+            className={styles.commentAt}
+            dangerouslySetInnerHTML={{ __html: comment.postSlug }}
+          />
+          <button onClick={() => handleDelete(comment.id)} className={styles.deleteButton}>
+            Delete
+          </button>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default AdminComments;
