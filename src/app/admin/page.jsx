49d2b4a67d3991be496f105e9dd99644ee52f6fa
Leopoldo <122998221+poldogs@users.noreply.github.com>
2024-01-13 21:40:26 +0100

    commit

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import styles from './adminPage.module.css';
 import AdminPosts from '../../components/adminPosts/AdminPosts';
 import AdminComments from '../../components/adminComments/AdminComments';
 import AdminUsers from '../../components/adminUsers/AdminUsers';
 
 const AdminPage = () => {
-  const router = useRouter();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -14,7 +12,7 @@ const AdminPage = () => {
     if (password === "admin") {
       try {
           localStorage.setItem('isAuthenticated', 'true');
-          router.push('/admin');
+          window.location.reload();
       } catch (error) {
         console.error('Error fetching:', error);
       }
