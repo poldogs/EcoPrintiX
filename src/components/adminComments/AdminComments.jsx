d65b11f1c9216eaa0760a4ed117fd45d651577cf
Leopoldo <122998221+poldogs@users.noreply.github.com>
2024-01-13 19:49:17 +0100

    commit

--- src/components/adminComments/AdminComments.jsx
+++ src/components/adminComments/AdminComments.jsx
@@ -38,7 +38,6 @@ const AdminComments = () => {
           }
       
           setComments(comments.filter((comment) => comment.id !== id));
-          fetchData();
         } catch (error) {
           console.error('Error:', error);
         }
