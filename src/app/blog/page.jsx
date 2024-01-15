e18e4c12bcec49a3bc8c80070b6c4df2e6c82986
Leopoldo <122998221+poldogs@users.noreply.github.com>
2024-01-15 19:10:18 +0100

    commit

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,7 +5,7 @@ import Menu from "@/components/menu/Menu";
 const BlogPage = ({ searchParams }) => {
   const page = parseInt(searchParams.page) || 1;
   const cat = searchParams.get("cat").replace("?TopViews", "");
-
+  
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{cat} Blog</h1>
